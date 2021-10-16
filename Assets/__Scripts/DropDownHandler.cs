199c3b49ce3ffc699e0eb692ddbd470f0c6d4316
MoonlightS0 <suljaev.sasha@yandex.ru>
2021-10-16 11:19:15 +0300

    Update "Quality project"
    The project sorted materials by folders and translated comments from the class.

---
+++ Assets/__Scripts/DropDownHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDownHandler : MonoBehaviour   //A class that extends the capabilities of the drop-down list.It is necessary to select the difficulty through the settings menu.
+{
+
+    public Dropdown ddgamediff;
+    public static int diffValue { get; set; } //A variable for storing and passing values to the Basket class.
+
+    private void Start()
+    {
+        ddgamediff.onValueChanged.AddListener(delegate
+        {
+            ddgamediffValueChangedHappened(ddgamediff);
+        });
+    }
+
+    public void ddgamediffValueChangedHappened(Dropdown sender)
+    {
+        Debug.Log("Difficulty set :" + sender.value);
+        diffValue = sender.value;  //Assigning a value to a new variable for transmission.
+        Debug.Log("diffValue is =" + diffValue);
+    }
+}
